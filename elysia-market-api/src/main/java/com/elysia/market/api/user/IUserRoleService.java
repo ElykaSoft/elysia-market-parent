31af04cbe186621029744e5096dbe0e2d2ddbb57
ElysiaKafka <38758499+lucifermbbgh@users.noreply.github.com>
2024-01-25 23:58:42 +0800

    用户模块

---
+++ elysia-market-api/src/main/java/com/elysia/market/api/user/IUserRoleService.java
@@ -0,0 +1,13 @@
+package com.elysia.market.api.user;
+
+/**
+ * @BelongsProject: elysia-market-parent
+ * @BelongsPackage: com.elysia.market.api.user
+ * @Author: ElysiaKafka
+ * @CreateTime: 2024-01-25  23:31:27
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public interface IUserRoleService {
+
+}
--- elysia-market-api/src/main/java/com/elysia/market/api/user/IUserService.java
+++ elysia-market-api/src/main/java/com/elysia/market/api/user/IUserService.java
@@ -1,5 +1,10 @@
 package com.elysia.market.api.user;
 
+import com.elysia.common.pojo.common.Result;
+import com.elysia.common.pojo.dao.ElysiaUser;
+import com.elysia.common.pojo.dao.ElysiaUserInfo;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 /**
  * @BelongsProject: Elysia
  * @BelongsPackage: com.elysia.service.user
@@ -9,4 +14,52 @@ package com.elysia.market.api.user;
  * @Version: 1.0
  */
 public interface IUserService {
+    /**
+     * @description: 用户注册
+     * @author: ElysiaKafka
+     * @date: 2024/1/25 23:11
+     * @param: username
+     * @param: password
+     * @return: com.elysia.common.pojo.common.Result<com.elysia.common.pojo.dao.ElysiaUser>
+     **/
+    Result<ElysiaUser> register(String username, String password);
+
+    /**
+     * @description: 查询用户信息
+     * @author: ElysiaKafka
+     * @date: 2024/1/25 23:12
+     * @param: username
+     * @param: password
+     * @return: com.elysia.common.pojo.common.Result<com.elysia.common.pojo.dao.ElysiaUserInfo>
+     **/
+    Result<ElysiaUserInfo> queryUserInfo(String username, String password);
+
+    /**
+     * @description: 修改密码
+     * @author: ElysiaKafka
+     * @date: 2024/1/25 23:12
+     * @param: username
+     * @param: password
+     * @return: com.elysia.common.pojo.common.Result<com.elysia.common.pojo.dao.ElysiaUser>
+     **/
+    Result<ElysiaUser> updatePassword(String username, String password);
+
+    /**
+     * @description: 修改用户信息
+     * @author: ElysiaKafka
+     * @date: 2024/1/25 23:12
+     * @param: elysiaUserInfo
+     * @return: com.elysia.common.pojo.common.Result<com.elysia.common.pojo.dao.ElysiaUserInfo>
+     **/
+    Result<ElysiaUserInfo> updateUserInfo(ElysiaUserInfo elysiaUserInfo);
+
+    /**
+     * @description: 用户注销
+     * @author: ElysiaKafka
+     * @date: 2024/1/25 23:12
+     * @param: elysiaUser
+     * @return: com.elysia.common.pojo.common.Result
+     **/
+    Result deleteAccount(ElysiaUser elysiaUser);
+
 }
